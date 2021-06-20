9d77ec2baecab1892418cc786c948fb41ab55d8c
DongSubKim <dskim9218@gmail.com>
2021-06-20 23:55:49 +0900

    remove unnecessary files

--- vendor/github.com/cloudinary/cloudinary-go/config/api.go
+++
@@ -1,9 +0,0 @@
-package config
-
-// API defines the configuration for making requests to the Cloudinary API.
-type API struct {
-	UploadPrefix  string `default:"https://api.cloudinary.com"`
-	Timeout       int64  `default:"60"` // seconds
-	UploadTimeout int64
-	ChunkSize     int64 `default:"20000000"` //bytes
-}
